6f9c29e27e4fb7f128ba0c8ba81b7f076fdda48b
DEngled <englederdavid@yahoo.de>
2020-06-03 10:35:29 +0200

    better structure

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -1,4 +1,5 @@
-﻿using Lottery.model;
+﻿using Lottery.controller;
+using Lottery.model;
 using System;
 using System.Security.Cryptography.X509Certificates;
 
@@ -8,62 +9,9 @@ namespace Lottery
     {
         static void Main(string[] args)
         {
+            TicketController ticketController = new TicketController();
             LotteryTicket ticket = new LotteryTicket();
-
-            Boolean play = true;
-            while (play)
-            {
-                Boolean correct = false;
-                correct = takeGuess(ticket);
-                if (correct == true)
-                {
-                    Console.WriteLine("Correct! Thank you for playing!");
-                }
-                else
-                {
-                    Console.WriteLine("Another guess? (y/n)");
-                    string next = Console.ReadLine().ToLower();
-                    if (next == "n")
-                    {
-                        play = false;
-                        Console.WriteLine("Correct number would have been: " + ticket.lotteryNumber + "\nThank you for playing");
-                    }
-                }
-            }
-        }
-
-        public static Boolean takeGuess(LotteryTicket ticket)
-        {
-            Console.WriteLine("Please guess a 6-figure number:");
-            string guess = Console.ReadLine();
-            string correctGuess = "";
-            Boolean correct = true;
-            for (int i = 0; i < ticket.lotteryNumber.Length; i++)
-            {
-                char charG = guess[i];
-                char charT = ticket.lotteryNumber[i];
-                if (charG == charT)
-                {
-                    correctGuess = correctGuess + charG;
-                }
-                else
-                {
-                    correctGuess = correctGuess + "*";
-                    correct = false;
-                }
-            }
-            if (correct == true)
-            {
-                return true;
-            } 
-            else
-            {
-                Console.WriteLine("Your correct numbers\n" + correctGuess);
-                return false;
-
-            }
-
-            
+            ticketController.playGame(ticket);
         }
     }
 }
---
+++ Lottery/controller/TicketController.cs
@@ -0,0 +1,67 @@
+﻿using Lottery.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lottery.controller
+{
+    class TicketController
+    {
+        public void playGame(LotteryTicket ticket)
+        {
+            Boolean play = true;
+            while (play)
+            {
+                Boolean correct = false;
+                correct = takeGuess(ticket);
+                if (correct == true)
+                {
+                    Console.WriteLine("Correct! Thank you for playing!");
+                    play = false;
+                }
+                else
+                {
+                    Console.WriteLine("Another guess? (y/n)");
+                    string next = Console.ReadLine().ToLower();
+                    if (next == "n")
+                    {
+                        play = false;
+                        Console.WriteLine("Correct number would have been: " + ticket.lotteryNumber + "\nThank you for playing");
+                    }
+                }
+            }
+        }
+
+        private Boolean takeGuess(LotteryTicket ticket)
+        {
+            Console.WriteLine("Please guess a 6-figure number:");
+            string guess = Console.ReadLine();
+            string correctGuess = "";
+            Boolean correct = true;
+            for (int i = 0; i < ticket.lotteryNumber.Length; i++)
+            {
+                char charG = guess[i];
+                char charT = ticket.lotteryNumber[i];
+                if (charG == charT)
+                {
+                    correctGuess = correctGuess + charG;
+                }
+                else
+                {
+                    correctGuess = correctGuess + "*";
+                    correct = false;
+                }
+            }
+            if (correct == true)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Your correct numbers\n" + correctGuess);
+                return false;
+            }
+        }
+
+    }
+}
